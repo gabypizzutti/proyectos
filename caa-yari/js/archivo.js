c4fb258f172a4feb1809e990fb734306acd5ef49
gabypizzutti <gabrielapizzutti@gmail.com>
2022-06-03 20:00:38 -0300

    commit inicial todos los proyectos

---
+++ caa-yari/js/archivo.js
@@ -0,0 +1,58 @@
+// //se ve en pantalla apenas se abre el navegador
+// alert("una historia con raices");
+
+// //asigno una variable llamada nombre que el valor es gabriela
+// let nombre = "Gabriela";
+
+// //muestro en un alert Gabriela
+// alert(nombre);
+
+// //a la variable apellido le asigno un pedido del usuario mediante prompt
+// let apellido = prompt("ingresa tu apellido");
+
+// //le pido que me lo escriba en el documento, aparece en la pagina web
+// document.write(apellido);
+
+// //concateno dos variables y las comillas actuan de espacio
+// document.write(nombre + " " + apellido);
+
+// //se pueden hacer cuentas matematicas , en la pantalla se vera 20
+// let edad = 19 + 1;
+// let edad1 = 20 * 3; 
+// // muestra nombre apellido y edad
+// document.write(nombre + " " + apellido + " " + edad);
+// document.write(edad1);
+
+// /* tipo de datos
+// - numericos
+// -string - cadena de texto
+// -array - arreglo
+// -object - objeto
+// -boolean (true or false)
+// -undefined 
+// - null
+// -NaN
+// */
+
+// //tipo de datos numerico
+// let edadMayor = 19; // valor entero
+// let precio = 0.5 // valor con decimal o float
+
+// //tipo de dato string - cadena de texto
+// let nombreMenor = "Sofia";
+
+// // se puede concatenar 
+// let apellidoMenor = "Cañada";
+// document.write(nombreMenor + " " + apellidoMenor );
+
+// //escape de comillas supongamos que queresmo poner "falconMaster entre comillas"
+// // let texto = "Los mejores videos son de "FalconMasters" "; // tira error
+// //opcion 1 (usar comillas simples afuera)
+// let texto1 = 'Los mejores videos son de "FalconMasters" '; 
+// console.log(texto1); // forma de ver en el inspeccionar, boton derecho inspeccionar o inpeccionar elemento
+// //opcion 2 (usar \"\")
+// let texto = "Los mejores videos son de \"Pizzu\" "; 
+// console.log(texto);
+// //opcion 3 (usar \'\')
+// let texto2 = "Los mejores videos son de \'Pizzu\' "; 
+// console.log(texto2);
